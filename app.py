4fcda8a10b80eef3e07130e8bbc31eab529985bb
GopinathRuthra <106552051+GopinathRuthra@users.noreply.github.com>
2022-06-30 15:29:55 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from unittest import result
-from flask import Flask, render_template,redirect,url_for, request , jsonify
+from flask import Flask, flash, render_template,redirect,url_for, request , jsonify, json
 import sqlite3
 
 app=Flask(__name__)
@@ -12,27 +12,13 @@ def homepage():
 def index():
     return render_template("index.html")
 
-@app.route('/view')
+@app.route('/display')
 def view():
-    con = sqlite3.connect("Asset.db")
+    con = sqlite3.connect("empass.db")
     cur = con.cursor()  
-    cur.execute("select * from Data")  
+    cur.execute("select * from details")  
     rows = cur.fetchall()  
-    # print(rows)
     return render_template("display.html",rows = rows) 
-
-@app.route('/delete', methods=['POST','GET'])
-def delete():
-    if request.method=="POST":
-        id = request.form.get('GID')
-        con = sqlite3.connect("Asset.db") 
-        cur = con.cursor()  
-        cur.execute("delete from Data where GID = ?",[id])
-        con.commit()
-        print("deleted successfully")
-        return redirect(url_for('view'))
-    else:
-        return render_template("delete.html")
 
 
 @app.route('/display', methods=['POST','GET'])
@@ -62,6 +48,8 @@ def display():
         s=request.form.get('DOB')
         t=request.form.get('Work_Location')
         u=request.form.get('P_Team')
+        if not u:
+            u = "Not Applicable"
         v=request.form.get('Vaccine_Action_Plan')
         w=request.form.get('Vaccinated_Status')
         x=request.form.get('Vaccination_Certificate_Uploaded')
@@ -75,16 +63,28 @@ def display():
         f1=request.form.get('Transport')
         g1=request.form.get('Distance')
         h1=request.form.get('Pickup')
+        if not h1:
+            h1 = "Not Applicable"
         i1=request.form.get('Address')
         j1=request.form.get('City_Pin')        
         k1=request.form.get('Native_State')
         l1=request.form.get('Native_District')
         m1=request.form.get('Type_of_Asset')
         n1=request.form.get('CPU_Laptop')
+        if not n1:
+            n1 = "Not Applicable"
         o1=request.form.get('Docking_Station')
+        if not o1:
+            o1 = "Not Applicable"
         p1=request.form.get('Monitor_1')
+        if not p1:
+            p1 = "Not Applicable"
         q1=request.form.get('Monitor_2')
+        if not q1:
+            q1 = "Not Applicable"
         r1=request.form.get('Desk_No')
+        if not r1:
+            r1 = "Not Applicable"
         s1=request.form.get('Dual_Moniter_SetUp')
         t1=request.form.get('Internet_availability')
         u1=request.form.get('Head_Phone_availability')
@@ -96,33 +96,54 @@ def display():
         a2=request.form.get('Spike_Buster')
 
         try:  
-            with sqlite3.connect("Asset.db") as con:
+            with sqlite3.connect("empass.db") as con:
                 print("Connected")    
                 cur = con.cursor()  
                 print("Got cursor")  
-                cur.execute("INSERT into Data (GID, Emp_Name, Band, CCName, CCID, DOJ, W_Email, Manager, Band_Change_Date, Lead, AGM_VP, Job_Role, T_Name, SPOC, Core_Process, Sub_process, Higher_Qualification, GENDER, DOB, Work_Location, P_Team, Vaccine_Action_Plan, Vaccinated_Status, Vaccination_Certificate_Uploaded, Accomodation, Personal_Email, Insurance, ECName, ECNumber, Mob, Marital, Transport, Distance, Pickup, Address, City_Pin, Native_District, Native_State, Type_of_Asset, CPU_Laptop, Docking_Station, Monitor_1, Monitor_2, Desk_No, Dual_Moniter_SetUp, Internet_availability, Head_Phone_availability, Additional_Key_Board, Mouse, UPS, Proper_Table_availability, Proper_Chair_availability, Spike_Buster) values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)", (a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,a1,b1,c1,d1,e1,f1,g1,h1,i1,j1,k1,l1,m1,n1,o1,p1,q1,r1,s1,t1,u1,v1,w1,x1,y1,z1,a2))  
+                cur.execute("INSERT into details (GID, Emp_Name, Band, CCName, CCID, DOJ, W_Email, Manager, Band_Change_Date, Lead, AGM_VP, Job_Role, T_Name, SPOC, Core_Process, Sub_process, Higher_Qualification, GENDER, DOB, Work_Location, P_Team, Vaccine_Action_Plan, Vaccinated_Status, Vaccination_Certificate_Uploaded, Accomodation, Personal_Email, Insurance, ECName, ECNumber, Mob, Marital, Transport, Distance, Pickup, Address, City_Pin, Native_District, Native_State, Type_of_Asset, CPU_Laptop, Docking_Station, Monitor_1, Monitor_2, Desk_No, Dual_Moniter_SetUp, Internet_availability, Head_Phone_availability, Additional_Key_Board, Mouse, UPS, Proper_Table_availability, Proper_Chair_availability, Spike_Buster) values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)", (a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,a1,b1,c1,d1,e1,f1,g1,h1,i1,j1,k1,l1,m1,n1,o1,p1,q1,r1,s1,t1,u1,v1,w1,x1,y1,z1,a2))  
                 print("executed successfully")  
                 con.commit()  
                 print("Customer successfully Added")  
-                
-                # cur.execute("select * from Data")  
-                # rows = cur.fetchall()
-                # print(rows)  
+                flash("Record Added Successfully","success")
         except Exception as e:   
             con.rollback()
             print(e)  
             print("We can not add the customer to the list" ) 
+            flash("Error in Add Record")
         finally:  
             con.close()
-            return redirect(url_for('view')) 
+            return redirect(url_for('display')) 
 
 @app.route('/test/<gid>', methods=['GET'])
 def testfn(gid):
-    con = sqlite3.connect("Asset.db")
+    con = sqlite3.connect("workday.db")
     cur = con.cursor()  
-    cur.execute("select * from Data where GID = ?",[gid])  
+    cur.execute("select * from Employee_Details where GID = ?",[gid])  
     rows = cur.fetchall()
-    return jsonify(rows)  # serialize and use JSON headers
+    return jsonify(rows)  
+
+@app.route('/display', methods=['GET','POST'])
+def updatedisplay():
+    Emp_Name = request.form['Emp_Name']
+    Band = request.form['Band']
+    CCName = request.form['CCName']
+    CCID = request.form['CCID']
+
+    con = sqlite3.connect("workday.db")
+    cur = con.cursor()
+
+    if Emp_Name == 'Emp_Name':
+        cur.execute("UPDATE employee SET Emp_Name = %s WHERE id = %s ", (Emp_Name, Emp_Name))
+    elif Band == 'Band':
+        cur.execute("UPDATE employee SET Band = %s WHERE id = %s ", (Band, Band))
+    elif CCName == 'CCName':
+        cur.execute("UPDATE employee SET CCName = %s WHERE id = %s ", (CCName, CCName))
+    elif CCID == 'CCID':
+        cur.execute("UPDATE employee SET CCID = %s WHERE id = %s ", (CCID, CCID))
+        
+    con.commit() 
+
+    return json.dumps({'status':'OK'})
 
 if __name__=='__main__':
     app.run(debug=True)
